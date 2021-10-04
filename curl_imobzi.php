9863e71035856432b419c8cfcf4167df7560e60c
Ricardo Gomes <RicardoGomes335@users.noreply.github.com>
2021-10-04 00:01:36 -0300

    Update curl_imobzi.php

--- curl_imobzi.php
+++ curl_imobzi.php
@@ -19,8 +19,24 @@
   // Mostrando o resultado
   curl_close($ch);
   $json = json_decode($season_data, true);
-  echo "<pre>";
-  print_r($json);
-  echo "</pre>";
+  //echo "<pre>";
+  //print_r($json);
+  //echo "</pre>";
+
+  //echo ("Bairro: ".$json['properties'][0]['neighborhood']);
+
+  //neste for percorre os produtos, e utiliza count para saber quantos tem
+    for ($i = 0; $i < count($json); ++$i){ 
+      echo "<pre>";
+      echo $json['properties'][$i]['neighborhood'];
+      echo "</pre>";
+      //usar a $teste[$i]['produtos']['descricao'];
+      //usar o $teste[$i]['produtos']['valor'];
+
+      //foreach aqui percorre as várias opções
+      //foreach ($json['properties'][$i]['neighborhood'] as $opcoes){
+            //echo $opcoes;
+          //}
+   }
 
 ?>
